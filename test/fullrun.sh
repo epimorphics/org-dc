cd3371aed2b7ef35c9b957f632619f584164e8e9
der <dave@epimorphics.com>
2014-05-20 17:12:04 +0100

    Various template bug fixes, defra example looking reasonably complete

---
+++ test/fullrun.sh
@@ -0,0 +1,1 @@
+ curl -F "release=2011-09-30" -F "senior-csv=@government-staff-and-salary-data-template---October-2011---Defra---Revised-senior-data.csv" -F "junior-csv=@government-staff-and-salary-data-template---October-2011---Defra---Revised-junior-data.csv" http://localhost:8080/convert > result.ttl
